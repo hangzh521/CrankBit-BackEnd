64f635feda3aa7ed2e349d4e4dbb515a5d12e79d
LingHan.S <LINGKUI0324@GMAIL.COM>
2023-06-30 11:38:53 +1000

    lint

--- src/utils/databaseManager.ts
+++ src/utils/databaseManager.ts
@@ -4,7 +4,7 @@ export default async function createDatabase(companyName: string): Promise<void>
   try {
     const dbName = `db_${companyName}`
 
-    await mongoose.connect('mongodb://localhost:27017/admin')
+    await mongoose.connect(process.env.MONGO_URI)
     await mongoose.connection.db.admin().command({ create: dbName })
     await mongoose.disconnect()
 
